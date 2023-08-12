6f7590992ebc43067685abc0676549ab3be89ec6
ATHVAR <119851369+ATHVAR@users.noreply.github.com>
2023-08-12 20:10:36 +0530

    Hosted in Cyclic

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.json());
+app.use(cors());
+const db = require("../Backend/db/index");
+const api=require("../Backend/router/router");
+app.use('/',api);
+const path = require('path');
+app.use(express.static('./Backend/frontend'));
+
+app.use(express.static(path.join(__dirname,'frontend')))
+
+app.get('*', async(req, res)=> {
+        res.sendFile(path.join(__dirname ,'frontend','index.html'))});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
